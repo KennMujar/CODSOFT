445df89303c0605f236d0cbb8d83c9dd272382a9
Kenn Mujar <gertrude.kenn.mujar@gmail.com>
2024-07-06 08:26:49 +0800

    splash screen

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.todolistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //changes 1
+    }
+}
