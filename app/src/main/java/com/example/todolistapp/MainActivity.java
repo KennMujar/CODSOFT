560115f28710d1699d76a15e8f4412d7204cac99
Kenn Mujar <gertrude.kenn.mujar@gmail.com>
2024-07-07 19:15:29 +0800

    Added checkbox

--- app/src/main/java/com/example/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.todolistapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.todolistapp.Adapter.ToDoAdapter;
+import com.example.todolistapp.Model.ToDoModel;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //changes 1
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        tasksList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        tasksList.add(task);
+        tasksAdapter.setTasks(tasksList);
     }
 }
